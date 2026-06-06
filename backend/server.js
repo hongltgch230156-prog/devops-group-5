c7295daac1e3b71d08994247e4fc640ffdc2f1fa
honglee <hongltgch230156@fpt.edu.vn>
2026-06-06 23:40:38 +0700

    Update frontend UI and notifications

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,11 @@ app.use(cors());
 app.use(express.json());
 
 // BUG #1: Wrong default password - doesn't match docker-compose!
+console.log('DB_HOST=', process.env.DB_HOST);
+console.log('DB_USER=', process.env.DB_USER);
+console.log('DB_NAME=', process.env.DB_NAME);
+console.log('DB_PORT=', process.env.DB_PORT);
+console.log('DB_PASSWORD=', process.env.DB_PASSWORD);
 const pool = new Pool({
    user: process.env.DB_USER,
    host: process.env.DB_HOST,
